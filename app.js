878952c3f977c3de960f005235d3dce3da702995
Rohan Kumar <62876849+RohanKumar110@users.noreply.github.com>
2022-03-03 10:31:58 +0500

    fix bug

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ function checkWinLose(guess, tiles) {
     stopInteraction();
     return;
   }
-  const remainingTiles = guessGrid.querySelectorAll("not([data-letter])");
+  const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])");
   if (remainingTiles.length === 0) {
     showAlert(targetWord.toUpperCase(), null);
     stopInteraction();
